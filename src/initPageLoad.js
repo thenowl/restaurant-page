854c6b115c3215be044f1457e9b8bbf0a199aece
thenowl <183875071+thenowl@users.noreply.github.com>
2025-01-08 07:53:32 +0100

    Further build the main page

--- src/initPageLoad.js
+++ src/initPageLoad.js
@@ -2,6 +2,14 @@ export function initPageLoad() {
   const contentContainer = document.querySelector("#content");
 
   const headlineContainer = document.createElement("div");
-  const headline = document.createElement("h1");
-  headline.textContent = "";
+  headlineContainer.classList.add("headlineContainer");
+  const greeting = document.createElement("h2");
+  greeting.classList.add("greeting");
+  greeting.textContent = "Bienvenue à";
+  headlineContainer.appendChild(greeting);
+  const restaurantName = document.createElement("h2");
+  restaurantName.classList.add("restaurantName");
+  restaurantName.textContent = "peu à peu";
+  headlineContainer.appendChild(restaurantName);
+  contentContainer.appendChild(headlineContainer);
 }
